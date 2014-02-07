83a4e14e3cfe07f5fb3b9dc933b63e880af8851c
jussijartamo <jussi@jartamo@gofore.com>
2014-02-07 14:20:20 +0200

    cors filter dokumenttipalveluun

--- dokumenttipalvelu-service/src/main/java/fi/vm/sade/valinta/dokumenttipalvelu/resource/impl/ResourceConfiguration.java
+++ dokumenttipalvelu-service/src/main/java/fi/vm/sade/valinta/dokumenttipalvelu/resource/impl/ResourceConfiguration.java
@@ -1,5 +1,11 @@
 package fi.vm.sade.valinta.dokumenttipalvelu.resource.impl;
 
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -9,17 +15,30 @@ import org.glassfish.jersey.server.ResourceConfig;
  * 
  */
 public class ResourceConfiguration extends ResourceConfig {
-    //
-    public ResourceConfiguration() {
-        // packages("fi.vm.sade.valinta.dokumenttipalvelu.resource.impl.DokumenttiResourceImpl");
-        // json output and input
-        register(JacksonFeature.class);
-        // register(MultiPartFeature.class);
+	//
+	public ResourceConfiguration() {
+		// packages("fi.vm.sade.valinta.dokumenttipalvelu.resource.impl.DokumenttiResourceImpl");
+		// json output and input
+		register(JacksonFeature.class);
+		// register(MultiPartFeature.class);
 
-        register(DokumenttiResourceImpl.class);
+		register(DokumenttiResourceImpl.class);
+		/**
+		 * CORS Filter
+		 */
+		register(new ContainerResponseFilter() {
 
-        registerInstances(new com.wordnik.swagger.jaxrs.listing.ResourceListingProvider(),
-                new com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider());
-        register(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
-    }
+			@Override
+			public void filter(ContainerRequestContext requestContext,
+					ContainerResponseContext responseContext)
+					throws IOException {
+				responseContext.getHeaders().add("Access-Control-Allow-Origin",
+						"*");
+			}
+		});
+		registerInstances(
+				new com.wordnik.swagger.jaxrs.listing.ResourceListingProvider(),
+				new com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider());
+		register(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+	}
 }
