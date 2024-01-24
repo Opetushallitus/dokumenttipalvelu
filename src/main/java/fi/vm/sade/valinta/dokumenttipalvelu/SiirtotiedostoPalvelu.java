d32ceda3aacd4c7fd458471d59b7be4a18dca76f
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2024-01-24 15:50:25 +0200

    OK-424: Siirtotiedosto API refactored; timestamps now given as strings, subCategory is a plain string as well

--- src/main/java/fi/vm/sade/valinta/dokumenttipalvelu/SiirtotiedostoPalvelu.java
+++ src/main/java/fi/vm/sade/valinta/dokumenttipalvelu/SiirtotiedostoPalvelu.java
@@ -4,10 +4,11 @@ import fi.vm.sade.valinta.dokumenttipalvelu.dto.ObjectMetadata;
 import software.amazon.awssdk.utils.StringUtils;
 
 import java.io.InputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,31 +16,37 @@ import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 
+
 public class SiirtotiedostoPalvelu extends Dokumenttipalvelu {
+    final DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+            .appendPattern("[yyyy-MM-dd'T'HH:mm:ss]")
+            .appendPattern("[dd.MM.yyyy HH.mm.ss]")
+            .toFormatter();
+
     public SiirtotiedostoPalvelu(String awsRegion, String bucketName) {
         super(awsRegion, bucketName);
     }
 
     private String timeRangeString(
-            final Optional<Date> timeRangeStart, final Optional<Date> timeRangeEnd) {
-        final DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy_HH.mm.ss");
+            final Optional<LocalDateTime> timeRangeStart, final Optional<LocalDateTime> timeRangeEnd) {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy_HH.mm.ss");
         if (timeRangeStart.isPresent() && timeRangeEnd.isPresent()) {
             return String.format(
-                    "%s-%s_", dateFormat.format(timeRangeStart.get()), dateFormat.format(timeRangeEnd.get()));
+                    "%s-%s_", formatter.format(timeRangeStart.get()), formatter.format(timeRangeEnd.get()));
         }
         if (timeRangeStart.isPresent()) {
-            return String.format("%s-_", dateFormat.format(timeRangeStart.get()));
+            return String.format("%s-_", formatter.format(timeRangeStart.get()));
         }
         if (timeRangeEnd.isPresent()) {
-            return String.format("-%s_", dateFormat.format(timeRangeEnd.get()));
+            return String.format("-%s_", formatter.format(timeRangeEnd.get()));
         }
         return "";
     }
 
-    private Collection<String> tags(String sourceSystem, Optional<String> subCategory) {
-        return subCategory.isPresent()
-                ? Arrays.asList(sourceSystem, subCategory.get())
-                : Arrays.asList(sourceSystem);
+    private Collection<String> tags(String sourceSystem, String subCategory) {
+        return StringUtils.isEmpty(subCategory)
+                ? Arrays.asList(sourceSystem)
+                : Arrays.asList(sourceSystem, subCategory);
     }
 
     @Override
@@ -50,29 +57,41 @@ public class SiirtotiedostoPalvelu extends Dokumenttipalvelu {
     /**
      * Saves siirtotiedosto document.
      *
-     * @param timeRangeStart Startpoint of the data timerange contained by this siirtotiedosto
+     * @param timeRangeStart Startpoint of the data timerange contained by this siirtotiedosto,
+     *                       allowed formats: '2024-01-24T10:15:30', '24.01.2024 10.15.30'.
+     *                       Value is optional, can be null or empty.
      * @param timeRangeEnd   Endpoint of the data timerange contained by this siirtotiedosto
-     * @param sourceSystem   Source system of this siirtotiedosto, e.g. kouta, ataru, etc
+     *                       allowed formats: '2024-01-24T10:15:30', '24.01.2024 10.15.30'.
+     *                       Value is optional, can be null or empty.
+     * @param sourceSystem   Source system of this siirtotiedosto, e.g. kouta, ataru, etc. Value is mandatory.
      * @param subCategory    More detailed description of the contents
-     * @param data           Document's data input stream
+     *                       Value is optional, can be null or empty.
+     * @param data           Document's data input stream. Value is mandatory.
      * @return Metadata describing the document.
      */
     public ObjectMetadata saveSiirtotiedosto(
-            final Optional<Date> timeRangeStart,
-            final Optional<Date> timeRangeEnd,
+            final String timeRangeStart,
+            final String timeRangeEnd,
             final String sourceSystem,
-            final Optional<String> subCategory,
+            final String subCategory,
             final InputStream data) {
 
         if (StringUtils.isEmpty(sourceSystem)) {
             throw new IllegalArgumentException("Source system cannot be empty");
         }
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+        Optional<LocalDateTime> timeRangeStartVal = StringUtils.isEmpty(timeRangeStart) ?
+                Optional.empty() : Optional.of(LocalDateTime.parse(timeRangeStart, dateTimeFormatter));
+        Optional<LocalDateTime> timeRangeEndVal = StringUtils.isEmpty(timeRangeEnd) ?
+                Optional.empty() : Optional.of(LocalDateTime.parse(timeRangeEnd, dateTimeFormatter));
         final String categoryStr =
-                subCategory.isPresent() ? String.format("%s_%s_", sourceSystem, subCategory.get()) : "";
+                StringUtils.isEmpty(subCategory) ? "" : String.format("%s_%s_", sourceSystem, subCategory);
         final String documentId =
                 String.format(
                         "%s%s%s.json",
-                        categoryStr, timeRangeString(timeRangeStart, timeRangeEnd), UUID.randomUUID());
+                        categoryStr, timeRangeString(timeRangeStartVal, timeRangeEndVal), UUID.randomUUID());
         final Collection<String> tags = tags(sourceSystem, subCategory);
         // TODO Poista expirationDate siinä vaiheessa kun se poistuu save -metodista
         final Date expirationDate = Date.from(Instant.now().plus(Duration.of(5, ChronoUnit.DAYS)));
